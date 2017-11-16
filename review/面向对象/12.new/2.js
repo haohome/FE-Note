657bb309395ff331245a63a2c742fdda4230da00
Daniel <wuhaoxcp@gmail.com>
2017-11-16 15:58:32 +0800

    first

---
+++ review/面向对象/12.new/2.js
@@ -0,0 +1,11 @@
+var person = {
+  sayName() {
+    console.log(this.name);
+  },
+  get firstName() {
+    return "Nicholas";
+  }
+};
+
+person.sayName.name   // "sayName"
+person.firstName.name // "get firstName"
